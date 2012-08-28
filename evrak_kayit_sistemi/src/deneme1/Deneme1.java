e9f9f5d55e8245c449a9473e571b1d492bb28100
cehmet <ankarali.ahmet.06@gmail.com>
2012-08-28 06:28:03 +0200

    evrak kayit sistemi

---
+++ evrak_kayit_sistemi/src/deneme1/Deneme1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deneme1;
+
+/**
+ *
+ * @author ahmet
+ */
+public class Deneme1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     girispenceresi  a=new girispenceresi();
+     a.setVisible(true);
+       
+    }
+}
